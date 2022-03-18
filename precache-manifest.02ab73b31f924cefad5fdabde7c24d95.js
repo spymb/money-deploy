53e6e279e203e44ff8d690d1d5389e5d7ad86472
spymb <spym1933@foxmail.com>
2022-03-18 19:56:35 +0800

    deployed

---
+++ precache-manifest.02ab73b31f924cefad5fdabde7c24d95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca8fb1a8c489a565f4e0",
+    "url": "/money-deploy/css/app.3ad3034a.css"
+  },
+  {
+    "revision": "ac056d5f0ac7e36d792f779a2695e383",
+    "url": "/money-deploy/index.html"
+  },
+  {
+    "revision": "ca8fb1a8c489a565f4e0",
+    "url": "/money-deploy/js/app.ded33b34.js"
+  },
+  {
+    "revision": "5184e585b2ba96ecf798",
+    "url": "/money-deploy/js/chunk-vendors.ad809f15.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-deploy/manifest.json"
+  },
+  {
+    "revision": "15df4e9537e35fc32c1910be3c915115",
+    "url": "/money-deploy/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-deploy/robots.txt"
+  }
+]);
